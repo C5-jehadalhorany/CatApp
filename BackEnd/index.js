36e29beb8c02b885dd6a33110aa26215d676c48a
jehadalhorany <alhoranyjehad@gmail.com>
2022-09-28 00:54:28 +0300

    add

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
+const cors = require("cors");
 require("./models/db");
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;;
 const CatsRouter = require("./routes/cats");
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 app.use("/cats", CatsRouter);
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 app.listen(PORT, () => {
